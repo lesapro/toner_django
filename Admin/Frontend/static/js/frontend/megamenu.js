03eb5379dfa7541546befc1730b1c7e59577d69f
Tuan Nguyen <ngoctuanit07@gmail.com>
2024-03-13 15:27:51 +0700

    Render catalog list ra trang chủ

--- Admin/Frontend/static/js/frontend/megamenu.js
+++ Admin/Frontend/static/js/frontend/megamenu.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     console.log("2");
     $('.categories-list .nav-center-images-only').slick({
         rows: 2,
@@ -8,7 +8,17 @@ $(document).ready(function() {
         speed: 300,
         slidesToShow: 5,
         slidesToScroll: 5,
-        draggable:true,
-        mobileFirst:true
+        draggable: true,
+        mobileFirst: true
+    })
+    $('.section-categories-list .categories-list-pc .nav-center-images-only').slick({
+        rows: 2,
+        dots: false,
+        arrows: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        draggable: true
     })
 })
