fc16661acbc59923a2040b5815ebf7ec801b633a
Tuan Nguyen <ngoctuanit07@gmail.com>
2024-03-15 14:12:34 +0700

    add product detail

--- Admin/Frontend/pages/views.py
+++ Admin/Frontend/pages/views.py
@@ -2,18 +2,27 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from collections import defaultdict
-from db.models import Product, Category
+from db.models import Product, Category,ProductName
 from django.shortcuts import get_object_or_404
-
+from django.urls import resolve
 # Create your views here.
 
 class PagesView(TemplateView):
-    def product_details_view(request, slug):
-        product = get_object_or_404(Product, name=lug)  # Giả sử 'name' là một trường ForeignKey hoặc OneToOneField và 'slug' là trường bạn muốn truy vấn
-        print(product)
-        context = {'product': product}
-        return render(request, 'product_details.html', context) 
-    pass 
+    def get_context_data(self, **kwargs):
+       context = super().get_context_data(**kwargs)
+       # Lấy current URL từ request
+       url = self.request.path_info
+        # Giải quyết URL
+       resolver_match = resolve(url)
+        # Lấy slug từ kwargs của resolver
+       product_slug = resolver_match.kwargs.get('slug')
+       product_name = ProductName.objects.get(slug=product_slug)
+       #print(product_name)
+       products = Product.objects.filter(name=product_name)
+       #print(product)
+       context['products'] = products
+       return context
+    pass
  
 # products
 
