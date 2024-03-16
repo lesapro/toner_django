d914119d1adfc8595903259fef252c13fd13daf0
MAC M1 <macm1@192.168.1.110>
2024-03-16 12:59:18 +0700

    api

--- Admin/Frontend/frontend/settings.py
+++ Admin/Frontend/frontend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'rest_framework',
     'compressor',
     'catalog',
     'dashboard',
