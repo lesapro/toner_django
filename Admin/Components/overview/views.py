4a3fac9af795596400b140b7a29c1313106b5dfa
root <root@localhost.localdomain>
2024-03-17 13:06:38 +0700

    q

--- Admin/Components/overview/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-
-# Create your views here.
-
-class overview(TemplateView):
-    pass 
-
-index_view = overview.as_view(template_name='overview/index.html')
