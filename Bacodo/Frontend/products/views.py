d6054a03e44af2600cd4c40cbf7ad98eae59984a
root <root@localhost.localdomain>
2024-03-18 21:58:40 +0700

    fix product detail

--- Bacodo/Frontend/products/views.py
+++ Bacodo/Frontend/products/views.py
@@ -1,28 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from collections import defaultdict
-from db.models import Product, Category, ProductName, ChildProduct, Color, Size, Option, Details
-from django.shortcuts import get_object_or_404
-from django.urls import resolve
-from django.db.models import Prefetch
-# Create your views here.
+from django.db.models import Prefetch, Subquery, OuterRef
+from db.models import Product, ProductName, ChildProduct, Color, Size, Option, Details
 
-class ProducrsView(TemplateView):
+class ProductsView(TemplateView):
+    template_name = 'products/product-details.html'
+
     def get_context_data(self, **kwargs):
-       context = super().get_context_data(**kwargs)
-       product_slug = self.kwargs.get('slug')
-       product_name = get_object_or_404(ProductName, slug=product_slug)
-       products = Product.objects.filter(name=product_name).prefetch_related(
-           Prefetch(
-               'child_products',
-               queryset=ChildProduct.objects.select_related('color', 'size', 'option', 'details')[:1]
-           )
-       )
-       context['products'] = products
-       return context
-    pass
- 
-# products
+        context = super().get_context_data(**kwargs)
+        product_slug = self.kwargs.get('slug')
+        product_name = get_object_or_404(ProductName, slug=product_slug)
 
-product_details_view = ProducrsView.as_view(template_name = 'products/product-details.html')
+        products = Product.objects.filter(name=product_name).prefetch_related(
+            'child_products__color',
+            'child_products__size',
+            'child_products__option',
+            'child_products__details'
+        )
+
+        # Đối với mỗi Product, lấy ChildProduct đầu tiên và gán vào thuộc tính mới
+        for product in products:
+            product.first_child_product = product.child_products.first() if product.child_products.exists() else None
+
+            if hasattr(product, 'gallery_image') and product.gallery_image:
+                product.gallery_images = product.gallery_image.split(',')
+            else:
+                product.gallery_images = []
+
+        context['products'] = products
+        return context
+
+product_details_view = ProductsView.as_view()
