54e689418ecbb123218f37522c7e3a6dd40ab5ef
root <root@localhost.localdomain>
2024-03-19 19:14:54 +0700

    update product detail

--- Bacodo/Frontend/products/views.py
+++ Bacodo/Frontend/products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
-from django.db.models import Prefetch, Subquery, OuterRef
-from db.models import Product, ProductName, ChildProduct, Color, Size, Option, Details
+from django.db.models import Prefetch
+from db.models import Product, ProductName, ChildProduct, Color, Size, Type, Option, Details, ShippingLocation
 
 class ProductsView(TemplateView):
     template_name = 'products/product-details.html'
@@ -11,24 +11,25 @@ class ProductsView(TemplateView):
         product_slug = self.kwargs.get('slug')
         product_name = get_object_or_404(ProductName, slug=product_slug)
 
-        products = Product.objects.filter(name=product_name).prefetch_related(
-            'child_products__color',
-            'child_products__size',
-            'child_products__option',
-            'child_products__details'
-        )
+        # Sử dụng Prefetch để tối ưu hóa việc truy vấn dữ liệu liên quan
+        child_prefetch = Prefetch('child_products', queryset=ChildProduct.objects.select_related('color', 'size', 'type', 'option', 'details', 'shipping_location'))
+        products = Product.objects.filter(name=product_name).prefetch_related(child_prefetch)
 
-        # Đối với mỗi Product, lấy ChildProduct đầu tiên và gán vào thuộc tính mới
         for product in products:
-            product.first_child_product = product.child_products.first() if product.child_products.exists() else None
+            # Gán giá và giá đặc biệt từ sản phẩm con đầu tiên
+            first_child = product.child_products.first()
+            product.first_child_price = first_child.price if first_child else None
+            product.first_child_special_price = first_child.special_price if first_child else None
 
-            if hasattr(product, 'gallery_image') and product.gallery_image:
-                product.gallery_images = product.gallery_image.split(',')
-            else:
-                product.gallery_images = []
+            # Tạo danh sách thuộc tính từ tất cả sản phẩm con
+            product.child_attributes = {
+                'colors': set(child.color.name for child in product.child_products.all() if child.color),
+                'sizes': set(child.size.name for child in product.child_products.all() if child.size),
+                'types': set(child.type.name for child in product.child_products.all() if child.type),
+                'options': set(child.option.name for child in product.child_products.all() if child.option),
+            }
 
         context['products'] = products
         return context
 
 product_details_view = ProductsView.as_view()
-
